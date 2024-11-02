bef848a2d479f56287ec11d977ef4b7d5c9818a8
MelnykOlena <melnyko1503@gmail.com>
2024-11-02 21:52:01 +0200

    Update example.js

--- example.js
+++ example.js
@@ -1,10 +1,10 @@
 const fs = require('fs');
 
-// Зчитуємо файл spaces.json
+// Зчитування файл spaces.json
 const data = fs.readFileSync('test-data/spaces.json', 'utf8');
 const json = JSON.parse(data);
 
-// Виводимо тільки space.name і space.id для lists, ім'я яких починається на "test"
+// Виведення space.name і space.id для lists, ім'я яких починається на "test"
 json.folders.forEach(folder => {
   folder.lists.forEach(list => {
     if (list.name.startsWith('test')) {
